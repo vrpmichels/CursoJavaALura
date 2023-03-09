af6350ac65e74c2088b0c2c9fdec1ba2afe6866c
DoveHeadGames <118575651+DoveHeadGames@users.noreply.github.com>
2023-03-09 14:47:53 -0300

    Aula sobre Wrappers
    Nessa aula focamos nas classes WRAPPERS e aprendemos que
    
    para cada primitivo existe uma classe chamada Wrapper
    para guardar um primitivo numa coleção é preciso criar um objeto que embrulha o valor
    a criação do objeto Wrapper é chamada de autoboxing
    a retirada do valor primitivo do objeto Wrapper é chamada de unboxing
    autoboxing e unboxing acontecem automaticamente.
    as classes wrapper possuem vários métodos auxiliares, por exemplo para o parsing
    todas as classes wrappers que representam um valor numérico possuem a classe java.lang.Number como mãe

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteExerciciosVini.java
@@ -0,0 +1,10 @@
+package br.com.bytebank.banco.test.util;
+
+public class TesteExerciciosVini {
+    public static void main(String[] args){
+        Integer ref = Integer.valueOf("3");
+        ref++;
+        System.out.println(ref);
+    }
+    
+}
