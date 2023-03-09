48dcc1ec4275ee01373e36126d670472b6aaac64
DoveHeadGames <118575651+DoveHeadGames@users.noreply.github.com>
2023-03-09 16:02:06 -0300

    Commit com novo aprendizado do curso - Java e java.util: Coleções, Wrappers e Lambda expressions
    Nessa aula fundamental e importante aprendemos que:
    
    para ordenar uma lista é preciso definir um critério de ordenação
    há duas formas de definir esse critério
    pela interface Comparator
    pela interface Comparable (ordem natural)
    o algoritmo de ordenação já foi implementado
    na lista no método sort
    na classe Collections pelo método sort
    a classe Collections é uma fachada com vários métodos auxiliares para trabalhar com as coleções, principalmente listas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -7,7 +7,7 @@ package br.com.bytebank.banco.modelo;
  * @author Nico Steppat
  *
  */
-public abstract class Conta extends Object {
+public abstract class Conta extends Object implements Comparable<Conta>{
 
     protected double saldo;
     private int agencia;
@@ -91,12 +91,15 @@ public abstract class Conta extends Object {
     public static int getTotal(){
         return Conta.total;
     }  
-    
 
+    @Override
+    public int compareTo(Conta outra){
+        return Double.compare(this.saldo, outra.saldo);
+    }
     
     @Override
 	public String toString() {
-		return "Numero: " + this.numero + ", Agencia: " + this.agencia;
+		return "Numero: " + this.numero + ", Agencia: " + this.agencia + ", Saldo: " + this.saldo;
 	}
 
     @Override
---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/TesteExerciciosViniDois.java
@@ -0,0 +1,53 @@
+package br.com.bytebank.banco.test.util;
+
+public class TesteExerciciosViniDois {
+    
+        public static void main(String[] args) {
+            Aula aulaIntro = new Aula("Introdução às Coleções", 20);
+            Aula aulaModelando = new Aula("Modelando a Classe Aula", 18);
+            Aula aulaSets = new Aula("Trabalhando com Conjuntos", 16);
+    
+            List<Aula> aulas = new ArrayList<Aula>();
+            aulas.add(aulaIntro);
+            aulas.add(aulaModelando);
+            aulas.add(aulaSets);
+    
+            Collections.sort(aulas);
+    
+            for (Aula aula : aulas) {
+                System.out.println(aula);
+            }
+        }
+    }
+    
+    class Aula {
+    
+        private String titulo;
+        private int tempo;
+    
+        public Aula(String titulo, int tempo) {
+            this.titulo = titulo;
+            this.tempo = tempo;
+        }
+    
+        public String getTitulo() {
+            return titulo;
+        }
+    
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+    
+        public int getTempo() {
+            return tempo;
+        }
+    
+        public void setTempo(int tempo) {
+            this.tempo = tempo;
+        }
+    
+        public String toString() {
+            return "[título: " + titulo + ", tempo: " + tempo + " minutos]";
+        }
+    }
+}
